cb2be57398a30d008a0e39f7efd565420109137f
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-16 11:11:47 +0530

    pk

---
+++ Day23AddressBookUseCase2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day23AddressBookUseCase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            //Create obj of class UseCase2
+            UseCase2 obj = new UseCase2();
+            do
+            {
+                Console.WriteLine("Insert Data : 1");
+                Console.WriteLine("Display Data : 2");
+                Console.WriteLine("Exit : 3");
+                Console.WriteLine("----------------");
+                Console.WriteLine("Enter Your Choice");
+                ch = int.Parse(Console.ReadLine());
+                //Switch case 
+                switch(ch)
+                {
+                    case 1:
+                        obj.insertData();
+                        break;
+                    case 2:
+                        obj.display();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter valid choice");
+                        break;
+
+                }
+            } while (ch != 3);
+        }
+    }
+}
---
+++ Day23AddressBookUseCase2/UseCase2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day23AddressBookUseCase2
+{
+    public class UseCase2
+    {
+        string fname, lname, state, city;
+        int zip, contact;
+        //Create ArrayList obj
+        ArrayList list = new ArrayList();
+        public void insertData()
+        {
+            //Taking input from user
+            Console.WriteLine("Enter First Name");
+            fname = Console.ReadLine();
+            list.Add(fname);
+            Console.WriteLine("Enter Last Name");
+            lname = Console.ReadLine();
+            list.Add(lname);
+            Console.WriteLine("Enter Contact");
+            contact = int.Parse(Console.ReadLine());
+            //Inser data in ArrayList using ArrayList object
+            list.Add(contact);
+            Console.WriteLine("Enter City");
+            city = Console.ReadLine();
+            list.Add(city);
+            Console.WriteLine("Enter State");
+            state = Console.ReadLine();
+            list.Add(city);
+            Console.WriteLine("Enter Zip");
+            zip = int.Parse(Console.ReadLine());
+            list.Add(zip);
+        }
+        public void display()
+        {
+            //Display ArrayList Data on console
+            foreach (var data in list)
+            {
+                Console.WriteLine(data);
+            }
+        }
+
+    }
+}
