febfc7b22813a0508bc8b99191f4463290891eb6
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-16 11:10:50 +0530

    pk

---
+++ Day23AddressBookUseCase1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day23AddressBookUseCase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create object of class UseCase1
+            UseCase1 obj = new UseCase1();
+            //Call methods using object
+            obj.insertData();
+            obj.display();
+        }
+    }
+}
