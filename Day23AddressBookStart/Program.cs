1938d41cdf4eeb2b07f68e108024cfeebec57427
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-16 11:06:13 +0530

    ok

---
+++ Day23AddressBookStart/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day23AddressBookStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start obj = new Start();
+                obj.Welcome();
+        }
+    }
+}
---
+++ Day23AddressBookStart/Start.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day23AddressBookStart
+{
+   public class Start
+    {
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome To Address Book");
+        }
+    }
+}
