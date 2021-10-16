72989a36b41b99b7d31f722917c9d8959bf85793
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-16 11:18:29 +0530

    pk

---
+++ Day23AddressBookUseCase10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day23AddressBookUseCase10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase10 obj = new UseCase10();
+            obj.insertData();
+            obj.display();
+        }
+    }
+}
---
+++ Day23AddressBookUseCase10/UseCase10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day23AddressBookUseCase10
+{
+    public class UseCase10
+    {
+        Dictionary<long, string> contact = new Dictionary<long, string>();
+        Dictionary<string, string> state = new Dictionary<string, string>();
+        public void insertData()
+        {
+            contact.Add(9359864239, "Vaibhav");
+            contact.Add(9359864240,"Pratibha");
+            contact.Add(9359864241,"Nita");
+            int a=contact.Count;
+            Console.WriteLine("Total contacts : " + a);
+        }
+        public void display()
+        {
+            foreach (var data in contact)
+            {
+
+                Console.WriteLine(data);
+            }
+            
+        }
+
+    }
+}
